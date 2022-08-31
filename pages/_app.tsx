b94bc3ae49accdd5a9b05f75cccac1901cb2e7ca
Sinh Tran <sinh.tran@terralogic.com>
2022-08-31 08:32:42 +0700

    setup nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { store } from '../app/store';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App=({
+  Component, pageProps,
+}: AppProps) => {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default App;
